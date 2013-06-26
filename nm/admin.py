dd41620f32b38aabd243c5301cf6a672926d3df5
xfynx <steewenson.evan@gmail.com>
2013-06-26 20:40:38 +0600

    admin's settings and other.. again

--- nm/admin.py
+++ nm/admin.py
@@ -1,5 +1,31 @@
 from django.contrib import admin
 from nm.models import *
 
-admin.site.register(Poll)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+    date_hierarchy = 'pub_date'
+
+admin.site.register(Poll, PollAdmin)
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Poll',               {'fields': ['poll']}),
+        ('Vote information', {'fields': ['choice_text', 'votes']}),
+    ]
+
+
+admin.site.register(Choice, ChoiceAdmin)
